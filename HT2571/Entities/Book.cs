6f383d46d717067c24202d0b1aa6eb941fa53aa7
Daniil <rychenkov16@yandex.ru>
2025-01-31 23:34:21 +0300

    add entities

--- HT2571/Entities/Book.cs
+++ HT2571/Entities/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HT2571.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace HT2571.Entities
 {
     internal class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int Quantity { get; set; }
+
+        public List<User> Users { get; set; } = new List<User>();
+
+
     }
 }
--- HT2571/Entities/User.cs
+++ HT2571/Entities/User.cs
@@ -8,5 +8,10 @@ namespace HT2571.Entities
 {
     internal class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public List<Book> Books { get; set; } = new List<Book>();
     }
 }
