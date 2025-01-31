83a3ce87608ac4a01b11e0c4986c0e0e125aff42
Daniil <rychenkov16@yandex.ru>
2025-01-31 23:54:51 +0300

    add prograam cs

--- HT2571/Program.cs
+++ HT2571/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace HT2571
+﻿using HHT2571.Entities;
+using HT2571.Repositories;
+using HT2571.Views;
+
+namespace HW_25_7_1
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static UserRepository userRepository;
+        static BookRepository bookRepository;
+
+        public static MainView mainView;
+        public static EditDbView editDbView;
+        public static EditDbUsersView editDbUsersView;
+        public static EditDbBooksView editDbBooksView;
+        public static DbQueryView dbQueryView;
+        public static DbQueryUsersView dbQueryUsersView;
+        public static DbQueryBooksView dbQueryBooksView;
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            userRepository = new UserRepository();
+            bookRepository = new BookRepository();
+
+            mainView = new MainView();
+            editDbView = new EditDbView();
+            editDbUsersView = new EditDbUsersView(userRepository);
+            editDbBooksView = new EditDbBooksView(bookRepository);
+            dbQueryView = new DbQueryView();
+            dbQueryUsersView = new DbQueryUsersView(userRepository);
+            dbQueryBooksView = new DbQueryBooksView(bookRepository);
+
+
+            var flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Начать работу (нажмите 1)");
+                Console.WriteLine("Завершить работу (нажмите 2)");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            mainView.Show();
+                            break;
+                        }
+                    case "2":
+                        {
+                            flag = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Некорректная команда");
+                            break;
+                        }
+                }
+            }
+
         }
     }
 }
