b96c80af95cde32a05817b43ed5bfd52abf2c72a
Daniil <rychenkov16@yandex.ru>
2025-01-31 23:58:50 +0300

    add edit users db

--- HT2571/Views/EditDbUsersView.cs
+++ HT2571/Views/EditDbUsersView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HT2571.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,62 @@ using System.Threading.Tasks;
 
 namespace HT2571.Views
 {
-    internal class EditDbUsersView
+    public class EditDbUsersView
     {
+        UserRepository userRepository;
+        public EditDbUsersView(UserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Добавить нового пользователя (нажмите 1)");
+            Console.WriteLine("Удалить пользователя по Id (нажмите 2)");
+            Console.WriteLine("Удалить пользователя по Email (нажмите 3)");
+            Console.WriteLine("Обновить имя пользователя (нажмите 4)");
+            Console.WriteLine("Выдать книгу пользователю (нажмите 5)");
+            Console.WriteLine("Возврат книги пользователем (нажмите 6)");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        userRepository.AddUser();
+                        break;
+                    }
+                case "2":
+                    {
+                        userRepository.DeleteUserById();
+                        break;
+                    }
+                case "3":
+                    {
+                        userRepository.DeleteUserByEmail();
+                        break;
+                    }
+                case "4":
+                    {
+                        userRepository.UpdateUserNameById();
+                        break;
+                    }
+                case "5":
+                    {
+                        userRepository.UserTakeBook();
+                        break;
+                    }
+                case "6":
+                    {
+                        userRepository.UserReturnBook();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Некорректная команда");
+                        break;
+                    }
+            }
+
+        }
     }
 }
