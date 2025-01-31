ef9cac7ee1fa160d8547d5723bbc95b29792f7df
Daniil <rychenkov16@yandex.ru>
2025-01-31 23:57:32 +0300

    add query users

--- HT2571/Views/DbQueryUsersView.cs
+++ HT2571/Views/DbQueryUsersView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HT2571.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace HT2571.Views
 {
-    internal class DbQueryUsersView
+    public class DbQueryUsersView
     {
+        UserRepository userRepository;
+
+        public DbQueryUsersView(UserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Найти пользователя по Id (нажмите 1)");
+            Console.WriteLine("Показать всех пользователей (нажмите 2)");
+            Console.WriteLine("Поиск книги на руках у пользователя (нажмите 3)");
+            Console.WriteLine("Количество книг на руках у пользователя (нажмите 4)");
+
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        userRepository.FindById();
+                        break;
+                    }
+                case "2":
+                    {
+                        userRepository.FindAll();
+                        break;
+                    }
+                case "3":
+                    {
+                        userRepository.UserHasBook();
+                        break;
+                    }
+                case "4":
+                    {
+                        userRepository.UserBooksCount();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Некорректная команда");
+                        break;
+                    }
+            }
+        }
     }
 }
