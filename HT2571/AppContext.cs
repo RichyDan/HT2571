ed08da230149812274b03bea34410d20807a54f2
Daniil <rychenkov16@yandex.ru>
2025-01-31 23:37:09 +0300

    add appcontext

--- HT2571/AppContext.cs
+++ HT2571/AppContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HT2571.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace HT2571
 {
-    internal class AppContext
+    internal class AppContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-E5HJAV2L\SQLEXPRESS01;Database=HT2571;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
     }
 }
