18b3e0b44d357b835cb483ec522e56a4d86ec3e8
Daniil <rychenkov16@yandex.ru>
2025-01-31 23:53:08 +0300

    add user repos

--- HT2571/Repositories/UserRepository.cs
+++ HT2571/Repositories/UserRepository.cs
@@ -1,66 +1,350 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using HT2571.Exceptions;
 using HT2571.Entities;
-using HT2571.Repositories;
-using HT2571.Views;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace HT2571.Repositories
 {
-    class Program
+    public class UserRepository
     {
-        static UserRepository userRepository;
-        static BookRepository bookRepository;
-
-        public static MainView mainView;
-        public static EditDbView editDbView;
-        public static EditDbUsersView editDbUsersView;
-        public static EditDbBooksView editDbBooksView;
-        public static DbQueryView dbQueryView;
-        public static DbQueryUsersView dbQueryUsersView;
-        public static DbQueryBooksView dbQueryBooksView;
-        public static void Main(string[] args)
+        public void FindById()
         {
-            userRepository = new UserRepository();
-            bookRepository = new BookRepository();
-
-            mainView = new MainView();
-            editDbView = new EditDbView();
-            editDbUsersView = new EditDbUsersView(userRepository);
-            editDbBooksView = new EditDbBooksView(bookRepository);
-            dbQueryView = new DbQueryView();
-            dbQueryUsersView = new DbQueryUsersView(userRepository);
-            dbQueryBooksView = new DbQueryBooksView(bookRepository);
-
-
-            var flag = true;
-            while (flag)
+            Console.Write("Введите ID пользователя для поиска: ");
+            try
             {
-                Console.WriteLine("Начать работу (нажмите 1)");
-                Console.WriteLine("Завершить работу (нажмите 2)");
-
-                switch (Console.ReadLine())
+                bool result = int.TryParse(Console.ReadLine(), out int id);
+                if (!result)
+                    throw new WrongIdException();
+                using (var db = new AppContext())
                 {
-                    case "1":
-                        {
-                            mainView.Show();
-                            break;
-                        }
-                    case "2":
-                        {
-                            flag = false;
-                            break;
-                        }
-                    default:
-                        {
-                            Console.WriteLine("Некорректная команда");
-                            break;
-                        }
+                    var user = db.Users.Where(user => user.Id == id).FirstOrDefault();
                 }
             }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Введен неверный ID");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение {ex.Message}");
+            }
+        }
 
+        public void FindAll()
+        {
+            try
+            {
+                using (var db = new AppContext())
+                {
+                    var users = db.Users.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение {ex.Message}");
+            }
+        }
+
+        public void AddUser()
+        {
+            try
+            {
+                Console.Write("Введите имя нового пользователя: ");
+                var name = Console.ReadLine();
+
+                Console.Write("Введите Email нового пользователя: ");
+                var email = Console.ReadLine();
+
+                if (!new EmailAddressAttribute().IsValid(email))
+                    throw new WrongEmailException();
+
+
+                using (var db = new AppContext())
+                {
+                    var user = new User { Name = name, Email = email };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            catch (WrongEmailException)
+            {
+                Console.WriteLine("Некорректный Email");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение {ex.Message}");
+            }
+        }
+
+        public void DeleteUserById()
+        {
+            Console.Write($"Введите Id пользователя для удаления");
+
+            try
+            {
+                bool result = int.TryParse(Console.ReadLine(), out var id);
+                if (!result)
+                    throw new WrongIdException();
+
+                using (var db = new AppContext())
+                {
+                    var user = db.Users.Where(user => user.Id == id).FirstOrDefault();
+                    if (user == null)
+                        throw new UserNotFoundException();
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+            }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Некорректный Id");
+            }
+            catch (UserNotFoundException)
+            {
+                Console.WriteLine("Пользователь с такими данными не найден");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение {ex.Message}");
+            }
+        }
+
+        public void DeleteUserByEmail()
+        {
+            Console.Write($"Введите Email пользователя для удаления");
+
+            try
+            {
+                var email = Console.ReadLine();
+                if (!new EmailAddressAttribute().IsValid(email))
+                    throw new WrongEmailException();
+
+                using (var db = new AppContext())
+                {
+                    var user = db.Users.Where(user => user.Email == email).FirstOrDefault();
+                    if (user == null)
+                        throw new UserNotFoundException();
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+            }
+            catch (WrongEmailException)
+            {
+                Console.WriteLine("Некорректный Email");
+            }
+            catch (UserNotFoundException)
+            {
+                Console.WriteLine("Пользователь с такими данными не найден");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение {ex.Message}");
+            }
+        }
+
+        public void UpdateUserNameById()
+        {
+            Console.Write("Введите Id пользователя для обновления имени: ");
+
+            try
+            {
+                bool result = int.TryParse(Console.ReadLine(), out var id);
+                if (!result)
+                    throw new WrongIdException();
+
+                using (var db = new AppContext())
+                {
+                    var user = db.Users.Where(user => user.Id == id).FirstOrDefault();
+                    if (user == null)
+                        throw new UserNotFoundException();
+
+                    Console.Write("Введите новое имя пользователя: ");
+                    string newName = Console.ReadLine();
+
+                    user.Name = newName;
+                    db.SaveChanges();
+                }
+            }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Некорректный Id");
+            }
+            catch (UserNotFoundException)
+            {
+                Console.WriteLine("Пользователь с такими данными не найден");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение {ex.Message}");
+            }
+        }
+
+        public void UserTakeBook()
+        {
+            try
+            {
+                Console.Write("Введите Id пользователя, который хочет взять книгу: ");
+                bool resultUserId = int.TryParse(Console.ReadLine(), out int userId);
+                if (!resultUserId)
+                    throw new WrongIdException();
+
+                Console.Write("Введите Id книги, которую хочет взять пользователь");
+                bool resultBookId = int.TryParse(Console.ReadLine(), out int bookId);
+                if (!resultBookId)
+                    throw new WrongIdException();
+
+                using (var db = new AppContext())
+                {
+                    var user = db.Users.Include(u => u.Books).Where(user => user.Id == userId).FirstOrDefault();
+                    if (user == null)
+                        throw new UserNotFoundException();
+
+                    var book = db.Books.Where(book => book.Id == bookId).FirstOrDefault();
+                    if (book == null)
+                        throw new BookNotFoundException();
+
+                    user.Books.Add(book);
+                    book.Quantity--;
+                    db.SaveChanges();
+                }
+            }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Некорректно введен Id");
+            }
+            catch (UserNotFoundException)
+            {
+                Console.WriteLine("Пользователь c указанным Id не найден");
+            }
+            catch (BookNotFoundException)
+            {
+                Console.WriteLine("Книга с указанным Id не найдена");
+            }
+
+        }
+
+        public void UserReturnBook()
+        {
+            try
+            {
+                Console.Write("Введите Id пользователя, который хочет вернуть книгу: ");
+                bool resultUserId = int.TryParse(Console.ReadLine(), out int userId);
+                if (!resultUserId)
+                    throw new WrongIdException();
+
+                Console.Write("Введите Id книги, которую хочет вернуть пользователь: ");
+                bool resultBookId = int.TryParse(Console.ReadLine(), out int bookId);
+                if (!resultBookId)
+                    throw new WrongIdException();
+
+                using (var db = new AppContext())
+                {
+                    var user = db.Users.Include(u => u.Books).Where(user => user.Id == userId).FirstOrDefault();
+                    if (user == null)
+                        throw new UserNotFoundException();
+
+                    var book = db.Books.Where(book => book.Id == bookId).FirstOrDefault();
+                    if (book == null)
+                        throw new BookNotFoundException();
+
+                    if (!user.Books.Contains(book))
+                        Console.WriteLine("Данному пользователю не выдавалась эта книга");
+
+                    user.Books.Remove(book);
+                    book.Quantity++;
+                    db.SaveChanges();
+                }
+            }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Некорректно введен Id");
+            }
+            catch (UserNotFoundException)
+            {
+                Console.WriteLine("Пользователь c указанным Id не найден");
+            }
+            catch (BookNotFoundException)
+            {
+                Console.WriteLine("Книга с указанным Id не найдена");
+            }
+
+        }
+
+        public bool UserHasBook()
+        {
+            try
+            {
+                Console.Write("Введите Id пользователя: ");
+                bool resultUserId = int.TryParse(Console.ReadLine(), out int userId);
+                if (!resultUserId)
+                    throw new WrongIdException();
+
+                Console.Write("Введите Id книги: ");
+                bool resultBookId = int.TryParse(Console.ReadLine(), out int bookId);
+                if (!resultBookId)
+                    throw new WrongIdException();
+
+                using (var db = new AppContext())
+                {
+                    var book = db.Books.Where(b => b.Id == bookId).FirstOrDefault();
+                    if (book == null)
+                        throw new BookNotFoundException();
+                    var user = db.Users.Include(u => u.Books).Where(u => u.Id == userId).FirstOrDefault();
+                    if (user == null)
+                        throw new UserNotFoundException();
+                    var result = db.Users.Include(us => us.Books).Where(us => us.Id == userId).Any(u => u.Books.Contains(book));
+                    return result;
+                }
+            }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Некорректно введен Id");
+                return false;
+            }
+            catch (BookNotFoundException)
+            {
+                Console.WriteLine("Книга с указанным Id не найдена");
+                return false;
+            }
+            catch (UserNotFoundException)
+            {
+                Console.WriteLine("Пользователь указанным Id не найден");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение: {ex.Message}");
+                return false;
+            }
+        }
+
+        public int UserBooksCount()
+        {
+            try
+            {
+                Console.Write("Введите Id пользователя: ");
+                bool resultUserId = int.TryParse(Console.ReadLine(), out int userId);
+                if (!resultUserId)
+                    throw new WrongIdException();
+
+                using (var db = new AppContext())
+                {
+                    int result = db.Users.Include(us => us.Books).Where(u => u.Id == userId).Select(u => u.Books).Count();
+                    return result;
+                }
+            }
+            catch (WrongIdException)
+            {
+                Console.WriteLine("Некорректно введен Id");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение: {ex.Message}");
+                return 0;
+            }
         }
     }
 }
